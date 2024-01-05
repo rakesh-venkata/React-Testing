7cf4c573a82afa1af215f7aeab9bc88eda679c99
rakesh-venkata <maddinenirakesh123@gmail.com>
2024-01-05 12:32:50 +0530

    comments added

--- src/test.utils.tsx
+++ src/test.utils.tsx
@@ -1,12 +1,13 @@
 import { ReactJSXElement } from "@emotion/react/types/jsx-namespace"
 import { render } from "@testing-library/react"
-import userEvent from "@testing-library/user-event"
 
 import { BrowserRouter } from "react-router-dom"
 
 // test utils file
 export const renderWithRouter = (ui : ReactJSXElement, {route = '/'} = {}) => {
-    window.history.pushState({}, 'Test page', route)
+    window.history.pushState({}, 'Test page', route)  // used to manipulate browsers history stack.It allows you to change the URL of the current window without reloading.
+    //This history object is the underlying thing in navigating routes,and routers render content based on URLS.
+    //So to provide navigation functionality in tests, history object is needed.
   
     return {
       
